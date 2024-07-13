bc8e327e59a231541e43d82cee859dd9f0be53df
chris-trieu <christrieu07@gmail.com>
2024-07-13 17:54:33 +1000

    50-days Moving Average implementation

--- main.py
+++ main.py
@@ -2,17 +2,28 @@
 from AlgorithmImports import *
 # endregion
 
-class WellDressedLightBrownGoshawk(QCAlgorithm):
-
+class QuantLeague(QCAlgorithm):
     def initialize(self):
-        self.set_start_date(2023, 1, 2)
-        self.set_cash(100000)
-        self.add_equity("SPY", Resolution.MINUTE)
-        self.add_equity("BND", Resolution.MINUTE)
-        self.add_equity("AAPL", Resolution.MINUTE)
+        self.set_start_date(2020, 1, 1)
+        self.set_cash(1000000)
+        self.symbol = self.add_equity("SPY", Resolution.Daily).Symbol
+        self.fast_moving_average = self.sma(self.symbol, 50, Resolution.Daily)
+        self.last_action = None
 
-    def on_data(self, data: Slice):
-        if not self.portfolio.invested:
-            self.set_holdings("SPY", 0.33)
-            self.set_holdings("BND", 0.33)
-            self.set_holdings("AAPL", 0.33)
+    def on_data(self, data):
+        # Ensure we have enough data to calculate the moving average
+        if not self.fast_moving_average.is_ready:
+            return
+        
+        price = data[self.symbol].Close
+        # Buy if the price is above the moving average
+        if price > self.fast_moving_average.current.value:
+            if not self.portfolio.invested or self.last_action == "Sell":
+                self.set_holdings(self.symbol, 1)
+                self.last_action = "Buy"
+
+        # Sell if the price is below the moving average
+        elif price < self.fast_moving_average.current.value:
+            if self.portfolio.invested and self.last_action == "Buy":
+                self.liquidate(self.symbol)
+                self.last_action = "Sell"
